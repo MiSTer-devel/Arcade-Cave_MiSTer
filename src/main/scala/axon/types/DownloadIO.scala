ff58636441dc1cc4d8a390bedc0931b3a7073415
Joshua Bassett <hello@joshbassett.info>
2020-11-16 21:09:17 +1100

    Add DownloadIO class

---
+++ src/main/scala/axon/types/DownloadIO.scala
@@ -0,0 +1,67 @@
+/*
+ *    __   __     __  __     __         __
+ *   /\ "-.\ \   /\ \/\ \   /\ \       /\ \
+ *   \ \ \-.  \  \ \ \_\ \  \ \ \____  \ \ \____
+ *    \ \_\\"\_\  \ \_____\  \ \_____\  \ \_____\
+ *     \/_/ \/_/   \/_____/   \/_____/   \/_____/
+ *    ______     ______       __     ______     ______     ______
+ *   /\  __ \   /\  == \     /\ \   /\  ___\   /\  ___\   /\__  _\
+ *   \ \ \/\ \  \ \  __<    _\_\ \  \ \  __\   \ \ \____  \/_/\ \/
+ *    \ \_____\  \ \_____\ /\_____\  \ \_____\  \ \_____\    \ \_\
+ *     \/_____/   \/_____/ \/_____/   \/_____/   \/_____/     \/_/
+ *
+ *  https://joshbassett.info
+ *  https://twitter.com/nullobject
+ *  https://github.com/nullobject
+ *
+ *  Copyright (c) 2020 Josh Bassett
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package axon.types
+
+import chisel3._
+
+/** A flow control interface used to download data into the core. */
+class DownloadIO private extends Bundle {
+  /** Enable flag */
+  val enable = Input(Bool())
+  /** Write enable */
+  val wr = Input(Bool())
+  /** Flag to indicate that the device isn't ready to proceed with the request  */
+  val waitReq = Output(Bool())
+  /** Index */
+  val index = Input(UInt(DownloadIO.INDEX_WIDTH.W))
+  /** Address bus */
+  val addr = Input(UInt(DownloadIO.ADDR_WIDTH.W))
+  /** Data bus */
+  val dout = Input(Bits(DownloadIO.DATA_WIDTH.W))
+}
+
+object DownloadIO {
+  /** The width of the index */
+  val INDEX_WIDTH = 8
+  /** The width of the address bus */
+  val ADDR_WIDTH = 27
+  /** The width of the data bus */
+  val DATA_WIDTH = 8
+
+  def apply() = new DownloadIO
+}
