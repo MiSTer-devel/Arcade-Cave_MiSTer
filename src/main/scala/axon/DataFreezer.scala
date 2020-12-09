dc262076adb613452ebabd6854e3f7a41dfde6ce
Joshua Bassett <hello@joshbassett.info>
2020-12-09 20:43:30 +1100

    Add axon DataFreezer module

---
+++ src/main/scala/axon/DataFreezer.scala
@@ -0,0 +1,108 @@
+/*
+ *    __   __     __  __     __         __
+ *   /\ "-.\ \   /\ \/\ \   /\ \       /\ \
+ *   \ \ \-.  \  \ \ \_\ \  \ \ \____  \ \ \____
+ *    \ \_\\"\_\  \ \_____\  \ \_____\  \ \_____\
+ *     \/_/ \/_/   \/_____/   \/_____/   \/_____/
+ *    ______     ______       __     ______     ______     ______
+ *   /\  __ \   /\  == \     /\ \   /\  ___\   /\  ___\   /\__  _\
+ *   \ \ \/\ \  \ \  __<    _\_\ \  \ \  __\   \ \ \____  \/_/\ \/
+ *    \ \_____\  \ \_____\ /\_____\  \ \_____\  \ \_____\    \ \_\
+ *     \/_____/   \/_____/ \/_____/   \/_____/   \/_____/     \/_/
+ *
+ *  https://joshbassett.info
+ *  https://twitter.com/nullobject
+ *  https://github.com/nullobject
+ *
+ *  Copyright (c) 2020 Josh Bassett
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package axon
+
+import chisel3._
+import mem._
+
+/**
+ * Transfers a memory interface between clock domains.
+ *
+ * Signals transferred from the fast clock domain are stretched so that they can be latched by flops
+ * in the slow clock domain. Signals transferred from the slow clock domain are unchanged.
+ *
+ * The data freezer requires that the clock domain frequencies are phase-aligned integer multiples
+ * of each other. This ensures that the signals can be transferred between the clock domains without
+ * data loss.
+ *
+ * @param addrWidth The width of the address bus.
+ * @param dataWidth The width of the data bus.
+ */
+class DataFreezer(addrWidth: Int, dataWidth: Int) extends Module {
+  val io = IO(new Bundle {
+    /** Target clock domain */
+    val targetClock = Input(Clock())
+    /** Input port */
+    val in = Flipped(AsyncReadWriteMemIO(addrWidth, dataWidth))
+    /** Output port */
+    val out = AsyncReadWriteMemIO(addrWidth, dataWidth)
+  })
+
+  // Detect rising edges of the target clock
+  val clear = Util.sync(io.targetClock)
+
+  // Latch wait/valid signals
+  val waitReq = !Util.latch(!io.out.waitReq, clear)
+  val valid = Util.latch(io.out.valid, clear)
+
+  // Latch valid output data
+  val data = Util.latch(io.out.dout, io.out.valid, clear)
+
+  // Latch pending read request until the request has completed
+  val pendingRead = Util.latch(RegNext(io.in.rd && !io.out.waitReq), clear && RegNext(valid))
+
+  // Latch pending write request
+  val pendingWrite = Util.latch(RegNext(io.in.wr && !io.out.waitReq), clear)
+
+  // Connect I/O ports
+  io.in <> io.out
+
+  // Outputs
+  io.in.waitReq := waitReq
+  io.in.valid := valid
+  io.in.dout := data
+  io.out.rd := io.in.rd && !pendingRead
+  io.out.wr := io.in.wr && !pendingWrite
+
+  printf(p"DataFreezer(read: ${io.out.rd}, write: ${io.out.wr}, wait: $waitReq, valid: $valid, clear: $clear)\n")
+}
+
+object DataFreezer {
+  /**
+   * Wraps the given memory interface with a data freezer.
+   *
+   * @param targetClock The target clock domain.
+   * @param mem The memory interface.
+   */
+  def freeze(targetClock: Clock)(mem: AsyncReadWriteMemIO): AsyncReadWriteMemIO = {
+    val freezer = Module(new DataFreezer(mem.addrWidth, mem.dataWidth))
+    freezer.io.targetClock := targetClock
+    freezer.io.out <> mem
+    freezer.io.in
+  }
+}
---
+++ src/test/scala/axon/DataFreezerTest.scala
@@ -0,0 +1,171 @@
+/*
+ *    __   __     __  __     __         __
+ *   /\ "-.\ \   /\ \/\ \   /\ \       /\ \
+ *   \ \ \-.  \  \ \ \_\ \  \ \ \____  \ \ \____
+ *    \ \_\\"\_\  \ \_____\  \ \_____\  \ \_____\
+ *     \/_/ \/_/   \/_____/   \/_____/   \/_____/
+ *    ______     ______       __     ______     ______     ______
+ *   /\  __ \   /\  == \     /\ \   /\  ___\   /\  ___\   /\__  _\
+ *   \ \ \/\ \  \ \  __<    _\_\ \  \ \  __\   \ \ \____  \/_/\ \/
+ *    \ \_____\  \ \_____\ /\_____\  \ \_____\  \ \_____\    \ \_\
+ *     \/_____/   \/_____/ \/_____/   \/_____/   \/_____/     \/_/
+ *
+ *  https://joshbassett.info
+ *  https://twitter.com/nullobject
+ *  https://github.com/nullobject
+ *
+ *  Copyright (c) 2020 Josh Bassett
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ */
+
+package axon
+
+import chisel3._
+import chiseltest._
+import chiseltest.experimental.UncheckedClockPoke._
+import org.scalatest._
+
+class DataFreezerTest extends FlatSpec with ChiselScalatestTester with Matchers {
+  private def mkFreezer = new DataFreezer(addrWidth = 8, dataWidth = 8)
+
+  behavior of "read"
+
+  it should "latch a read request" in {
+    test(mkFreezer) { dut =>
+      // Read
+      dut.io.in.rd.poke(true.B)
+      dut.io.out.waitReq.poke(true.B)
+      dut.clock.step()
+
+      // Wait
+      dut.io.out.waitReq.poke(false.B)
+      dut.io.out.rd.expect(true.B)
+      dut.clock.step()
+      dut.io.out.waitReq.poke(true.B)
+      dut.io.out.rd.expect(false.B)
+      dut.clock.step()
+
+      // Rising edge
+      dut.io.targetClock.low()
+      dut.io.targetClock.high()
+      dut.io.out.rd.expect(false.B)
+      dut.clock.step()
+
+      // Valid
+      dut.io.out.valid.poke(true.B)
+      dut.io.out.rd.expect(false.B)
+      dut.clock.step()
+      dut.io.out.valid.poke(false.B)
+      dut.io.out.rd.expect(false.B)
+
+      // Rising edge
+      dut.io.targetClock.low()
+      dut.io.targetClock.high()
+      dut.io.out.rd.expect(true.B)
+    }
+  }
+
+  it should "latch valid output data" in {
+    test(mkFreezer) { dut =>
+      // Read
+      dut.io.in.rd.poke(true.B)
+      dut.io.out.valid.poke(true.B)
+      dut.io.out.dout.poke(1.U)
+      dut.clock.step()
+      dut.io.out.dout.poke(0.U)
+      dut.io.in.dout.expect(1.U)
+      dut.clock.step()
+
+      // Rising edge
+      dut.io.targetClock.low()
+      dut.io.targetClock.high()
+      dut.io.in.dout.expect(0.U)
+    }
+  }
+
+  behavior of "write"
+
+  it should "latch a write request" in {
+    test(mkFreezer) { dut =>
+      // Write
+      dut.io.in.wr.poke(true.B)
+      dut.io.out.waitReq.poke(true.B)
+      dut.clock.step()
+
+      // Wait
+      dut.io.out.waitReq.poke(false.B)
+      dut.io.out.wr.expect(true.B)
+      dut.clock.step()
+      dut.io.out.waitReq.poke(true.B)
+      dut.io.out.wr.expect(false.B)
+      dut.clock.step()
+
+      // Rising edge
+      dut.io.targetClock.low()
+      dut.io.targetClock.high()
+      dut.io.out.wr.expect(true.B)
+    }
+  }
+
+  behavior of "wait"
+
+  it should "latch the wait signal" in {
+    test(mkFreezer) { dut =>
+      // Assert wait
+      dut.io.out.waitReq.poke(true.B)
+      dut.io.in.waitReq.expect(true.B)
+      dut.clock.step()
+
+      // Deassert wait
+      dut.io.out.waitReq.poke(false.B)
+      dut.io.in.waitReq.expect(false.B)
+      dut.clock.step()
+      dut.io.out.waitReq.poke(true.B)
+      dut.io.in.waitReq.expect(false.B)
+
+      // Rising edge
+      dut.io.targetClock.low()
+      dut.io.targetClock.high()
+      dut.io.in.waitReq.expect(true.B)
+    }
+  }
+
+  behavior of "valid"
+
+  it should "latch the valid signal" in {
+    test(mkFreezer) { dut =>
+      // Assert valid
+      dut.io.out.valid.poke(true.B)
+      dut.io.in.valid.expect(true.B)
+      dut.clock.step()
+
+      // Deassert valid
+      dut.io.out.valid.poke(false.B)
+      dut.io.in.valid.expect(true.B)
+      dut.clock.step()
+      dut.io.in.valid.expect(true.B)
+
+      // Rising edge
+      dut.io.targetClock.low()
+      dut.io.targetClock.high()
+      dut.io.in.valid.expect(false.B)
+    }
+  }
+}
