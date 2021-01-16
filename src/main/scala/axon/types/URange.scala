09b5dc4ac5dccabd7b0f89bca9b9c5bba86e6928
Joshua Bassett <hello@joshbassett.info>
2021-01-16 15:59:51 +1100

    Add URange bundle

---
+++ src/main/scala/axon/types/URange.scala
@@ -0,0 +1,62 @@
+/*
+ *   __   __     __  __     __         __
+ *  /\ "-.\ \   /\ \/\ \   /\ \       /\ \
+ *  \ \ \-.  \  \ \ \_\ \  \ \ \____  \ \ \____
+ *   \ \_\\"\_\  \ \_____\  \ \_____\  \ \_____\
+ *    \/_/ \/_/   \/_____/   \/_____/   \/_____/
+ *   ______     ______       __     ______     ______     ______
+ *  /\  __ \   /\  == \     /\ \   /\  ___\   /\  ___\   /\__  _\
+ *  \ \ \/\ \  \ \  __<    _\_\ \  \ \  __\   \ \ \____  \/_/\ \/
+ *   \ \_____\  \ \_____\ /\_____\  \ \_____\  \ \_____\    \ \_\
+ *    \/_____/   \/_____/ \/_____/   \/_____/   \/_____/     \/_/
+ *
+ * https://joshbassett.info
+ * https://twitter.com/nullobject
+ * https://github.com/nullobject
+ *
+ * Copyright (c) 2021 Josh Bassett
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package axon.types
+
+import chisel3._
+
+/**
+ * Represents an unsigned integer range.
+ *
+ * @param w The width of the start/end range value.
+ */
+class URange(w: Int) extends Bundle {
+  /** Start value */
+  val from = UInt(w.W)
+  /** End value */
+  val to = UInt(w.W)
+
+  override def cloneType: this.type = new URange(w).asInstanceOf[this.type]
+}
+
+object URange {
+  def apply(r: Range) = {
+    val from = r.start.U
+    val to = r.last.U
+    val w = Math.max(from.getWidth, to.getWidth)
+    val wire = Wire(new URange(w))
+    wire.from := from
+    wire.to := to
+    wire
+  }
+}
+
