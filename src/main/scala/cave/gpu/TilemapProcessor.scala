a8b279776dd19ede5af510a65954a1f086fec7d2
Joshua Bassett <hello@joshbassett.info>
2022-04-09 19:47:02 +1000

    Rename TilemapProcessor cols/rows params to maxCols/maxRows

--- src/main/scala/cave/gpu/TilemapProcessor.scala
+++ src/main/scala/cave/gpu/TilemapProcessor.scala
@@ -43,10 +43,10 @@ import chisel3.util._
 /**
  * The tilemap processor is responsible for rendering tilemap layers.
  *
- * @param cols The number of columns in the tilemap.
- * @param rows The number of rows in the tilemap.
+ * @param maxCols The maximum number of columns in the tilemap.
+ * @param maxRows The maximum number of rows in the tilemap.
  */
-class TilemapProcessor(cols: Int = 40, rows: Int = 30) extends Module {
+class TilemapProcessor(maxCols: Int = 40, maxRows: Int = 30) extends Module {
   val io = IO(new Bundle {
     /** Game config port */
     val gameConfig = Input(GameConfig())
@@ -105,8 +105,8 @@ class TilemapProcessor(cols: Int = 40, rows: Int = 30) extends Module {
   val fifo = Module(new Queue(Bits(Config.TILE_ROM_DATA_WIDTH.W), Config.FIFO_DEPTH, flow = true))
 
   // Columns/rows
-  val numCols = Mux(io.layer.tileSize, (cols / 2 + 1).U, (cols + 1).U)
-  val numRows = Mux(io.layer.tileSize, (rows / 2 + 1).U, (rows + 1).U)
+  val numCols = Mux(io.layer.tileSize, (maxCols / 2 + 1).U, (maxCols + 1).U)
+  val numRows = Mux(io.layer.tileSize, (maxRows / 2 + 1).U, (maxRows + 1).U)
 
   // Counters
   val (col, colWrap) = Counter.dynamic(numCols, enable = stateReg === State.next)
