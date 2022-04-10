14c6ce87a69c8e21f4393c6aec762888751adce1
atrac17 <32810066+atrac17@users.noreply.github.com>
2022-04-10 15:45:05 -0400

    v_irq to esprade

--- src/main/scala/cave/Cave.scala
+++ src/main/scala/cave/Cave.scala
@@ -396,8 +396,8 @@ class Cave extends Module {
       map(0x700000 to 0x707fff).readWriteMem(layer2Ram.io.portA)
       map(0x800000 to 0x80007f).writeMem(videoRegs.io.mem.asWriteMemIO)
       map(0x800000 to 0x800007).r { (_, offset) =>
-        when(offset === 0.U) { videoIRQ := false.B }
-        "b011".U ## !videoIRQ
+        when(offset === 4.U) { videoIRQ := false.B }
+        "b001".U ## !videoIRQ
       }
       map(0x900000 to 0x900005).readWriteMem(layer0Regs.io.mem)
       map(0xa00000 to 0xa00005).readWriteMem(layer1Regs.io.mem)
@@ -405,9 +405,8 @@ class Cave extends Module {
       map(0xc00000 to 0xc0ffff).readWriteMem(paletteRam.io.portA)
       map(0xd00010).r { (_, _) => input0 }
       map(0xd00012).r { (_, _) => input1 }
-      map(0xd00014).r { (_, _) => 0.U }
     }
-	
+
     // When the game is paused, request frames at the start of every vertical blank
     when(pauseReg) { frameStart := vBlank }
   }
