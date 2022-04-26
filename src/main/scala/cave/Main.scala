674dee21f815f8bc44e64a90b0b8b76872458fe1
Joshua Bassett <hello@joshbassett.info>
2022-04-26 18:17:53 +1000

    Rename ctrl port in DDR module

--- src/main/scala/cave/Main.scala
+++ src/main/scala/cave/Main.scala
@@ -104,7 +104,7 @@ class Main extends Module {
 
   // DDR controller
   val ddr = Module(new DDR(Config.ddrConfig))
-  ddr.io.ddr <> io.ddr
+  ddr.io.ctrl <> io.ddr
 
   // SDRAM controller
   val sdram = Module(new SDRAM(Config.sdramConfig))
