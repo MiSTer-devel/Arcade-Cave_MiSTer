6c1ffec81455bdbdd8220f82dab18aec39c648d7
Joshua Bassett <hello@joshbassett.info>
2022-05-19 19:36:07 +1000

    Update top-level copyright header
    This header was outdated. The whole file has been rewritten since the original VHDL version of DoDonPachi.

--- quartus/cave.sv
+++ quartus/cave.sv
@@ -1,22 +1,34 @@
-//============================================================================
-//  DoDonPachi for MiSTer.
-//  Copyright (c) 2019 Rick Wertenbroek
-//  Copyright (c) 2020 Josh Bassett
-//
-//  This program is free software; you can redistribute it and/or modify it
-//  under the terms of the GNU General Public License as published by the Free
-//  Software Foundation; either version 2 of the License, or (at your option)
-//  any later version.
-//
-//  This program is distributed in the hope that it will be useful, but WITHOUT
-//  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-//  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-//  more details.
-//
-//  You should have received a copy of the GNU General Public License along
-//  with this program; if not, write to the Free Software Foundation, Inc.,
-//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-//============================================================================
+/*
+ *   __   __     __  __     __         __
+ *  /\ "-.\ \   /\ \/\ \   /\ \       /\ \
+ *  \ \ \-.  \  \ \ \_\ \  \ \ \____  \ \ \____
+ *   \ \_\\"\_\  \ \_____\  \ \_____\  \ \_____\
+ *    \/_/ \/_/   \/_____/   \/_____/   \/_____/
+ *   ______     ______       __     ______     ______     ______
+ *  /\  __ \   /\  == \     /\ \   /\  ___\   /\  ___\   /\__  _\
+ *  \ \ \/\ \  \ \  __<    _\_\ \  \ \  __\   \ \ \____  \/_/\ \/
+ *   \ \_____\  \ \_____\ /\_____\  \ \_____\  \ \_____\    \ \_\
+ *    \/_____/   \/_____/ \/_____/   \/_____/   \/_____/     \/_/
+ *
+ * https://joshbassett.info
+ * https://twitter.com/nullobject
+ * https://github.com/nullobject
+ *
+ * Copyright (c) 2022 Josh Bassett
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 module emu (
   //Master input clock
@@ -184,7 +196,7 @@ localparam CONF_STR = {
   "-;",
   "R0,Reset;",
   "J,B0,B1,B2,B3,Start,Coin,Pause,Service;",
-  "V,v",`BUILD_DATE," - Made with love by nullobject.;"
+  "V,v",`BUILD_DATE," by nullobject;"
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -444,7 +456,7 @@ wire service_1         = key_9     | joystick_0[11];
 wire service_2         = key_0     | joystick_1[11];
 
 ////////////////////////////////////////////////////////////////////////////////
-// CAVE
+// MAIN
 ////////////////////////////////////////////////////////////////////////////////
 
 wire [31:0] ddr_addr;
