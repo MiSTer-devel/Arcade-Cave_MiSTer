ef39716e6ce50c70293b1b9ab5d9eb8719e62ed5
Joshua Bassett <hello@joshbassett.info>
2022-05-30 11:17:06 +1000

    Reorder Dangun Feveron ROMs

--- src/main/scala/cave/GameConfig.scala
+++ src/main/scala/cave/GameConfig.scala
@@ -103,17 +103,17 @@ object GameConfig {
     val wire = Wire(new GameConfig)
     wire.index := DFEVERON.U
     wire.progRomOffset := 0x00000000.U
-    wire.soundRomOffset := 0x00d00000.U
-    wire.eepromOffset := 0x01100000.U
+    wire.soundRomOffset := 0x00100000.U
+    wire.eepromOffset := 0x00500000.U
     wire.granularity := 16.U
     wire.sprite.format := Config.GFX_FORMAT_4BPP.U
     wire.sprite.zoom := true.B
-    wire.sprite.romOffset := 0x00100000.U
+    wire.sprite.romOffset := 0x00900080.U
     wire.layer(0).format := Config.GFX_FORMAT_4BPP.U
     wire.layer(1).format := Config.GFX_FORMAT_4BPP.U
     wire.layer(2).format := Config.GFX_FORMAT_UNKNOWN.U
-    wire.layer(0).romOffset := 0x00900000.U
-    wire.layer(1).romOffset := 0x00b00000.U
+    wire.layer(0).romOffset := 0x00500080.U
+    wire.layer(1).romOffset := 0x00700080.U
     wire.layer(2).romOffset := 0.U
     wire
   }
