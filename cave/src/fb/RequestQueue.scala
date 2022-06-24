9b2ef5d4abc71bf3b19422f9f6541bb16dc09f61
Joshua Bassett <hello@joshbassett.info>
2022-06-24 22:31:06 +1000

    Handle different RequestQueue word sizes

--- cave/src/fb/RequestQueue.scala
+++ cave/src/fb/RequestQueue.scala
@@ -35,11 +35,12 @@ package cave.fb
 import arcadia.mem._
 import cave._
 import chisel3._
+import chisel3.util._
 
 /**
  * Queues memory write requests and ensures they are eventually written to DDR memory.
  *
- * If the DDR memory is busy, then requests will be queued until they can be completed.
+ * If the DDR memory is busy, then requests will be queued until they can be processed.
  *
  * @param addrWidth The width of the address bus.
  * @param dataWidth The width of the data bus.
@@ -57,6 +58,9 @@ class RequestQueue(addrWidth: Int, dataWidth: Int, depth: Int) extends Module {
     val ddr = BurstWriteMemIO(Config.ddrConfig.addrWidth, Config.ddrConfig.dataWidth)
   })
 
+  // The number of words per DDR word
+  val dataWidthRatio = Config.ddrConfig.dataWidth / dataWidth
+
   // FIFO
   val fifo = Module(new DualClockFIFO(io.mem.getWidth, depth))
   fifo.io.readClock := io.readClock
@@ -70,7 +74,30 @@ class RequestQueue(addrWidth: Int, dataWidth: Int, depth: Int) extends Module {
   fifo.io.deq.ready := !io.ddr.waitReq
   val request = fifo.io.deq.bits.asTypeOf(new WriteMemIO(addrWidth, dataWidth))
   io.ddr.burstLength := 1.U
-  io.ddr.addr := request.addr ## 0.U(2.W)
-  io.ddr.mask := Mux(request.addr(0), request.mask ## 0.U(4.W), request.mask)
-  io.ddr.din := request.din ## request.din
+
+  // Outputs
+  io.ddr.addr := request.addr << (3 - log2Ceil(dataWidthRatio)) // convert to byte address
+  io.ddr.din := Cat(Seq.fill(dataWidthRatio) { request.din })
+
+  if (dataWidth == 8) {
+    io.ddr.mask := MuxLookup(request.addr(2, 0), request.mask, Seq(
+      1.U -> (request.mask << 1),
+      2.U -> (request.mask << 2),
+      3.U -> (request.mask << 3),
+      4.U -> (request.mask << 4),
+      5.U -> (request.mask << 5),
+      6.U -> (request.mask << 6),
+      7.U -> (request.mask << 7),
+    ))
+  } else if (dataWidth == 16) {
+    io.ddr.mask := MuxLookup(request.addr(1, 0), request.mask, Seq(
+      1.U -> (request.mask << 2),
+      2.U -> (request.mask << 4),
+      3.U -> (request.mask << 6),
+    ))
+  } else if (dataWidth == 32) {
+    io.ddr.mask := Mux(request.addr(0), request.mask << 4, request.mask)
+  } else if (dataWidth == 64) {
+    io.ddr.mask := request.mask
+  }
 }
